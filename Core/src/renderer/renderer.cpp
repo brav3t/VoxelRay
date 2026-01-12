6a767ac040eff7a49ce5e623d0688ca734bd8815
brav3T <bravetgm@gmail.com>
2026-01-12 19:57:01 +0100

    Add movement possiblity

--- Core/src/renderer/renderer.cpp
+++ Core/src/renderer/renderer.cpp
@@ -1,17 +1,16 @@
-
+﻿
 #pragma once
 
 #include "renderer.h"
 
-#include "../window_handling/window_factory.h"
-#include "index_buffer.h"
-#include "vertex_array.h"
-#include "shader.h"
-#include "ray_box_shader.h"
-
 #include "gl_utils.h"
+#include "../window_handling/window_factory.h"
+#include "ray_box_shader.h"
+#include "camera.h"
+
 #include <glad/glad.h>
 #include <glm/fwd.hpp>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <utility>
@@ -37,25 +36,72 @@ void Renderer::render()
         return;
     }
 
+    Camera camera;
+
+    double lastTime = glfwGetTime();
+    double lastX = 0.0, lastY = 0.0;
+    bool firstMouse = true;
+
     GLuint dummyVAO = 0;
     GLCall(glGenVertexArrays(1, &dummyVAO));
     GLCall(glBindVertexArray(dummyVAO));
 
     RayBoxShader voxels("res/shaders/raybox.shader");
 
-    glm::vec3 cameraPosition(0.0f, 0.0f, 3.0f);
-
-    glm::mat4 invViewProj(1.0f);
-
     GLCall(glViewport(0, 0, (int)mRenderWindow->resolution.x, (int)mRenderWindow->resolution.y));
+
+    GLCall(glClearColor(0.05f, 0.05f, 0.07f, 1.0f));
 
     while(!mRenderWindow->shouldClose())
     {
-        mRenderWindow->processInput();
+        auto* wnd = (GLFWwindow*)mRenderWindow->pWnd;
+
+        // Mouse look with right click
+
+        if (glfwGetMouseButton(wnd, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+        {
+            glfwSetInputMode(wnd, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+            double x, y;
+            glfwGetCursorPos(wnd, &x, &y);
+            if (firstMouse){ lastX = x; lastY = y; firstMouse = false; }
+
+            float dx = float(x - lastX);
+            float dy = float(lastY - y);
+            lastX = x; lastY = y;
+
+            const float sens = 0.08f;
+            camera.addPitchYaw(dx * sens, dy * sens);
+        }
+        else
+        {
+            glfwSetInputMode(wnd, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+            firstMouse = true;
+        }
+
+        // WASD move
+        float speed = 3.0f;
+        if (glfwGetKey(wnd, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) speed = 8.0f;
+
+        // Get time delta
+        double now = glfwGetTime();
+        float dt = float(now - lastTime);
+        lastTime = now;
+
+        if (glfwGetKey(wnd, GLFW_KEY_W) == GLFW_PRESS) camera.pos += camera.forward() * speed * dt;
+        if (glfwGetKey(wnd, GLFW_KEY_S) == GLFW_PRESS) camera.pos -= camera.forward() * speed * dt;
+        if (glfwGetKey(wnd, GLFW_KEY_A) == GLFW_PRESS) camera.pos -= camera.right() * speed * dt;
+        if (glfwGetKey(wnd, GLFW_KEY_D) == GLFW_PRESS) camera.pos += camera.right() * speed * dt;
+
+        // update invViewProj
+        float aspect = mRenderWindow->resolution.x / mRenderWindow->resolution.y;
+        glm::mat4 view = camera.view();
+        glm::mat4 proj = camera.proj(aspect);
+        glm::mat4 invViewProj = glm::inverse(proj * view);
 
         clear();
 
-        draw(voxels, cameraPosition, invViewProj, mRenderWindow->resolution);
+        draw(voxels, camera.pos, invViewProj, mRenderWindow->resolution);
 
         mRenderWindow->onUpdate();
     }
